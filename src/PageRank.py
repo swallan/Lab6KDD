e592b617b6f54caffa127325ff1920ef7b542743
Sam Wallan <44255917+swallan@users.noreply.github.com>
2021-11-23 14:17:45 -0800

    ok

--- src/PageRank.py
+++ src/PageRank.py
@@ -1,15 +1,20 @@
 import sys
-
+from scipy.sparse import csr_matrix as csr
 import numpy as np
+
 
 def load_data(fileName):
     conversions = dict()
-    idx = 1
+    idx = 0
     outData = []
     with open(fileName, 'r') as f:
         for ln in f.readlines():
             arr = ln.strip().replace('"', '').split(',')
-            print(arr)
+            arr = [x.replace(' ', '') for x in arr]
+            if "football" in fileName:
+                arr[0], arr[2] = arr[2], arr[0]
+                # need to swap the order since WINNING teams should come send
+
             if arr[0] in conversions.keys():
                 arr[0] = conversions[arr[0]]
             else:
@@ -24,59 +29,61 @@ def load_data(fileName):
                 idx += 1
                 arr[2] = conversions[arr[2]]
             outData.append(arr[:4])
-    print(outData)
     return np.asarray(outData, dtype=int), conversions
+
 
 def PageRank2(V):
     return np.ones(V.shape[0]) / V.shape[0]
 
+
 from numpy.linalg import norm
-def pageRank(G, d, eps):
+
+
+def pageRank(G: np.ndarray, d: float, eps: float) -> (list, int):
     n = G.shape[0]
     ranks = np.ones(n) / n
-    # M_weighted = M
-    # M_weighted[M_weighted > 0] = 1 / M_weighted[M_weighted > 0]
     r = 0
-    constant = (1 - d) * (1 / G.shape[0])
-    # constant2 = (1 - d) / G.shape[0])
-    # GT = G.T[...,::-1][::-1]
+    constant = ((1 - d) / G.shape[0])
     while True:
-        r = r + 1
-        print(r)
-        ranksP = (G @ ranks * d) + constant
-        if norm(ranksP - ranks) < eps**12:
-            return ranksP
+        r += 1
+        ranksP = ((G @ ranks) * d) + constant
+        if norm(ranks - ranksP) < eps:
+            return ranksP, r
         else:
             ranks = ranksP
-        if r > 10:
-            print("maxItr: " + str(r*3))
-            return(ranksP)
+
 
 if __name__ == '__main__':
     dataSource = sys.argv[1]
     d = float(sys.argv[2])
     eps = float(sys.argv[3])
-    print(f"Running PageRank with {dataSource}")
+    s = (f"""Running PageRank with {dataSource}
+d={d}, eps={eps}\n""")
     V, conversions = load_data(dataSource)
 
     allNodes = np.unique(np.concatenate((V[..., 0], V[..., 2])))
-
-
+    nNodes = len(allNodes)
     matrix = np.zeros((allNodes.shape[0], allNodes.shape[0]))
-    for i, _, j, _ in V:
-        matrix[j - 1, i - 1] += 1
+
+
+    csr_m = csr((np.ones(len(V)), (V[...,2], V[...,0])), shape=(nNodes, nNodes))
+
+    # for i, _, j, _ in V:
+    #     matrix[j, i] += 1
 
     counts = np.count_nonzero(matrix, axis=0)
-    counts[counts == 0] = -1
-    matrix = matrix * (1/counts)
-    print(matrix)
-    pageRanki = pageRank(matrix, d, eps)
-
+    counts[counts == 0] = 1
+    csr_m = csr_m.multiply(1 / counts)
+    pageRanki, niterations = pageRank(matrix, d, eps)
 
     sort_idx = np.argsort(pageRanki)[::-1]
     inv_map = {v: k for k, v in conversions.items()}
     ranks_sorted = pageRanki[sort_idx]
     names = [inv_map[x] for x in allNodes[sort_idx]]
+    s += f"After n={niterations} iterations:\n"
     for i, name in enumerate(names[:10]):
-        print(f"{ranks_sorted[i]:<.08f} : {name}")
-    # print(pageRanki)
+        s += f"{ranks_sorted[i]:<.08f} : {name} {i}\n"
+    s += f"np.sum(pageRanks) = {np.sum(pageRanki):.10f}\n"
+    print(s)
+    with open(f"out/pageRank_d{d}_eps{eps}_dense.txt", 'w') as f:
+        f.write(s)
