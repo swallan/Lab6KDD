d022a44b509b8245c18146064b874ea1b17aa461
Sam Wallan <44255917+swallan@users.noreply.github.com>
2021-11-23 14:45:40 -0800

    this works, sparse result included

--- src/PageRank.py
+++ src/PageRank.py
@@ -6,6 +6,7 @@ import numpy as np
 
 
 def load_data(fileName):
+    print("Loading data...", end="")
     conversions = dict()
     idx = 0
     outData = []
@@ -13,6 +14,11 @@ def load_data(fileName):
         for ln in f.readlines():
             arr = ln.strip().replace('"', '').split(',')
             arr = [x.replace(' ', '') for x in arr]
+            if ("wiki-Vote.txt" in fileName or "p2p-Gnutella05" in fileName
+                    or 'amazon' in fileName
+                    or 'soc-sign-Slashdot081106' in fileName):
+                l = ln.replace("\n", '').split('\t')
+                arr = [l[0], 0, l[1], 0]
             if "football" in fileName:
                 arr[0], arr[2] = arr[2], arr[0]
                 # need to swap the order since WINNING teams should come send
@@ -31,6 +37,7 @@ def load_data(fileName):
                 idx += 1
                 arr[2] = conversions[arr[2]]
             outData.append(arr[:4])
+    print("done.")
     return np.asarray(outData, dtype=int), conversions
 
 
@@ -42,16 +49,19 @@ from numpy.linalg import norm
 
 
 def pageRank(G: np.ndarray, d: float, eps: float) -> (list, int):
+    print("Running pagerank....", end="")
     n = G.shape[0]
     ranks = np.ones(n) / n
     r = 0
     constant = ((1 - d) / G.shape[0])
     t0 = time.time()
     while True:
+        if r % 100 == 0:
+            print(f"iteration: {r}")
         r += 1
         ranksP = ((G @ ranks) * d) + constant
         if norm(ranks - ranksP) < eps:
-            print(f"time elapsed: {time.time() - t0:.08f}")
+            print(f"done. time elapsed: {time.time() - t0:.08f}")
             return ranksP, r
         else:
             ranks = ranksP
@@ -68,19 +78,19 @@ d={d}, eps={eps}\n""")
 
     allNodes = np.unique(np.concatenate((V[..., 0], V[..., 2])))
     nNodes = len(allNodes)
-    matrix = np.zeros((allNodes.shape[0], allNodes.shape[0]))
+    # matrix = np.zeros((allNodes.shape[0], allNodes.shape[0]))
 
 
     csr_m = csr((np.ones(len(V)), (V[...,2], V[...,0])), shape=(nNodes, nNodes))
 
-    for i, _, j, _ in V:
-        matrix[j, i] += 1
+    # for i, _, j, _ in V:
+    #     matrix[j, i] += 1
 
     # counts = np.count_nonzero(matrix, axis=0)
     sp_matrix = csr_m.tocoo()
     col_ind = sp_matrix.col
     counts, unqiues = np.unique(col_ind, return_counts=1)
-    all_ones = np.ones(324)
+    all_ones = np.ones(sp_matrix.shape[0])
     all_ones[counts] = unqiues
 
     csr_m = csr_m.multiply(1 / all_ones)
@@ -96,5 +106,5 @@ d={d}, eps={eps}\n""")
         s += f"{ranks_sorted[i]:<.08f} : {name} {i}\n"
     s += f"np.sum(pageRanks) = {np.sum(pageRanki):.10f}\n"
     print(s)
-    with open(f"out/pageRank_d{d}_eps{eps}_dense.txt", 'w') as f:
+    with open(f"out/pageRank_d{d}_eps{eps}_sparse_{dataSource.split('/')[-1].replace('.txt', '')}.txt", 'w') as f:
         f.write(s)
