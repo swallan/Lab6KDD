958c276041c0d942fb1e5be839bac13f525c0fa3
Sam Wallan <44255917+swallan@users.noreply.github.com>
2021-11-23 14:49:14 -0800

    comments, adjustments

--- src/PageRank.py
+++ src/PageRank.py
@@ -2,10 +2,10 @@ import sys
 import time
 
 from scipy.sparse import csr_matrix as csr
+from numpy.linalg import norm
 import numpy as np
 
-
-def load_data(fileName):
+def load_data(fileName: str):
     print("Loading data...", end="")
     conversions = dict()
     idx = 0
@@ -19,6 +19,7 @@ def load_data(fileName):
                     or 'soc-sign-Slashdot081106' in fileName):
                 l = ln.replace("\n", '').split('\t')
                 arr = [l[0], 0, l[1], 0]
+
             if "football" in fileName:
                 arr[0], arr[2] = arr[2], arr[0]
                 # need to swap the order since WINNING teams should come send
@@ -41,13 +42,6 @@ def load_data(fileName):
     return np.asarray(outData, dtype=int), conversions
 
 
-def PageRank2(V):
-    return np.ones(V.shape[0]) / V.shape[0]
-
-
-from numpy.linalg import norm
-
-
 def pageRank(G: np.ndarray, d: float, eps: float) -> (list, int):
     print("Running pagerank....", end="")
     n = G.shape[0]
@@ -67,7 +61,6 @@ def pageRank(G: np.ndarray, d: float, eps: float) -> (list, int):
             ranks = ranksP
 
 
-
 if __name__ == '__main__':
     dataSource = sys.argv[1]
     d = float(sys.argv[2])
@@ -78,25 +71,25 @@ d={d}, eps={eps}\n""")
 
     allNodes = np.unique(np.concatenate((V[..., 0], V[..., 2])))
     nNodes = len(allNodes)
-    # matrix = np.zeros((allNodes.shape[0], allNodes.shape[0]))
 
+    # form sparse matrix. Format: data is all ones,
+    # data are from input with (data, (row_idx, col_idx))), then shape
+    csr_m = csr((np.ones(len(V)), (V[..., 2], V[..., 0])),
+                shape=(nNodes, nNodes))
 
-    csr_m = csr((np.ones(len(V)), (V[...,2], V[...,0])), shape=(nNodes, nNodes))
-
-    # for i, _, j, _ in V:
-    #     matrix[j, i] += 1
-
-    # counts = np.count_nonzero(matrix, axis=0)
+    # calculate the counts per column, then adjust the probabilities
+    # inside the matrix
     sp_matrix = csr_m.tocoo()
     col_ind = sp_matrix.col
     counts, unqiues = np.unique(col_ind, return_counts=1)
     all_ones = np.ones(sp_matrix.shape[0])
     all_ones[counts] = unqiues
-
     csr_m = csr_m.multiply(1 / all_ones)
-    # matrix = matrix * (1/counts)
+
+    # calculate pagerank
     pageRanki, niterations = pageRank(csr_m, d, eps)
 
+    # print results
     sort_idx = np.argsort(pageRanki)[::-1]
     inv_map = {v: k for k, v in conversions.items()}
     ranks_sorted = pageRanki[sort_idx]
@@ -106,5 +99,7 @@ d={d}, eps={eps}\n""")
         s += f"{ranks_sorted[i]:<.08f} : {name} {i}\n"
     s += f"np.sum(pageRanks) = {np.sum(pageRanki):.10f}\n"
     print(s)
-    with open(f"out/pageRank_d{d}_eps{eps}_sparse_{dataSource.split('/')[-1].replace('.txt', '')}.txt", 'w') as f:
+    with open(
+            f"out/pageRank_d{d}_eps{eps}_sparse_{dataSource.split('/')[-1].replace('.txt', '')}.txt",
+            'w') as f:
         f.write(s)
