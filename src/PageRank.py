eaf84f54cf05113c1fd13cfa8647bc2911fe2e98
Sam Wallan <44255917+swallan@users.noreply.github.com>
2021-11-23 16:53:26 -0800

    add results

--- src/PageRank.py
+++ src/PageRank.py
@@ -6,40 +6,52 @@ from numpy.linalg import norm
 import numpy as np
 
 def load_data(fileName: str):
+    t0 = time.time()
     print("Loading data...", end="")
     conversions = dict()
     idx = 0
     outData = []
     with open(fileName, 'r') as f:
         for ln in f.readlines():
-            arr = ln.strip().replace('"', '').split(',')
-            arr = [x.replace(' ', '') for x in arr]
-            if ("wiki-Vote.txt" in fileName or "p2p-Gnutella05" in fileName
-                    or 'amazon' in fileName
-                    or 'soc-sign-Slashdot081106' in fileName):
-                l = ln.replace("\n", '').split('\t')
-                arr = [l[0], 0, l[1], 0]
+            idx += 1
+            if idx % 1000000 == 0:
+                print(f"{idx / 68993773 * 100:.04f}%")
+            if '#' not in ln:
+                arr = ln.strip().replace('"', '').split(',')
+                arr = [x.replace(' ', '') for x in arr]
+                if ("wiki-Vote.txt" in fileName or "p2p-Gnutella05" in fileName
+                        or 'amazon' in fileName
+                        or 'soc-sign-Slashdot081106' in fileName
+                        or 'LiveJournal1' in fileName
+                        ):
+                    l = ln.replace("\n", '').split('\t')
+                    arr = [int(l[0]), 0, int(l[1]), 0]
 
-            if "football" in fileName:
-                arr[0], arr[2] = arr[2], arr[0]
-                # need to swap the order since WINNING teams should come send
+                if "football" in fileName:
+                    arr[0], arr[2] = arr[2], arr[0]
+                    # need to swap the order since WINNING teams should come send
 
-            if arr[0] in conversions.keys():
-                arr[0] = conversions[arr[0]]
+                # if arr[0] in conversions.keys():
+                #     arr[0] = conversions[arr[0]]
+                # else:
+                #     conversions[arr[0]] = idx
+                #     idx += 1
+                #     arr[0] = conversions[arr[0]]
+                #
+                # if arr[2] in conversions.keys():
+                #     arr[2] = conversions[arr[2]]
+                # else:
+                #     conversions[arr[2]] = idx
+                #     idx += 1
+                #     arr[2] = conversions[arr[2]]
+                conversions[arr[2]] = arr[2]
+                conversions[arr[0]] = arr[0]
+                outData.append(arr[:4])
             else:
-                conversions[arr[0]] = idx
-                idx += 1
-                arr[0] = conversions[arr[0]]
+                print(ln, end="")
 
-            if arr[2] in conversions.keys():
-                arr[2] = conversions[arr[2]]
-            else:
-                conversions[arr[2]] = idx
-                idx += 1
-                arr[2] = conversions[arr[2]]
-            outData.append(arr[:4])
     print("done.")
-    return np.asarray(outData, dtype=int), conversions
+    return np.asarray(outData, dtype=int), conversions, time.time() - t0
 
 
 def pageRank(G: np.ndarray, d: float, eps: float) -> (list, int):
@@ -56,7 +68,7 @@ def pageRank(G: np.ndarray, d: float, eps: float) -> (list, int):
         ranksP = ((G @ ranks) * d) + constant
         if norm(ranks - ranksP) < eps:
             print(f"done. time elapsed: {time.time() - t0:.08f}")
-            return ranksP, r
+            return ranksP, r, time.time() - t0
         else:
             ranks = ranksP
 
@@ -67,7 +79,7 @@ if __name__ == '__main__':
     eps = float(sys.argv[3])
     s = (f"""Running PageRank with {dataSource}
 d={d}, eps={eps}\n""")
-    V, conversions = load_data(dataSource)
+    V, conversions, dataLoadTime = load_data(dataSource)
 
     allNodes = np.unique(np.concatenate((V[..., 0], V[..., 2])))
     nNodes = len(allNodes)
@@ -87,13 +99,14 @@ d={d}, eps={eps}\n""")
     csr_m = csr_m.multiply(1 / all_ones)
 
     # calculate pagerank
-    pageRanki, niterations = pageRank(csr_m, d, eps)
+    pageRanki, niterations, processingTime = pageRank(csr_m, d, eps)
 
     # print results
     sort_idx = np.argsort(pageRanki)[::-1]
     inv_map = {v: k for k, v in conversions.items()}
     ranks_sorted = pageRanki[sort_idx]
     names = [inv_map[x] for x in allNodes[sort_idx]]
+    s += f"readTime: {dataLoadTime:.02f}s, processTime: {processingTime:.02f}s\n"
     s += f"After n={niterations} iterations:\n"
     for i, name in enumerate(names[:10]):
         s += f"{ranks_sorted[i]:<.08f} : {name} {i}\n"
